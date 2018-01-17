f28c9fb1e164803e21ae7f6c812652efa1e16efe
Jeremy Batten <jeremy@ablerz.nl>
2018-01-17 10:41:45 +0100

    Adding files for the theme starter for drupal 8 with gulp and bootstrap 4

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    babel = require("gulp-babel"),
+    minify = require("gulp-babel-minify"),
+    concat = require('gulp-concat');
+
+
+// task Imagemin, Compile images
+
+gulp.task('imagemin', function () {
+    return gulp.src('./src/images/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./assets/images'));
+});
+
+// Compile sass into CSS & auto-inject into browsers
+
+gulp.task('sass', function() {
+    return gulp.src(['./node_modules/bootstrap/scss/bootstrap.scss',
+        './src/scss/**/*.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(concat('styles.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest("./assets/css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Compile JS files
+
+gulp.task('script', function () {
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+        './src/js/**/*.js'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat('all.js'))
+        .pipe(minify({
+            mangle: {
+                keepClassName: true
+            }
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./assets/js'))
+        .pipe(browserSync.stream());
+});
+
+
+// Static Server + watching scss/html files
+
+gulp.task('watch', ['sass', 'script', 'imagemin'], function() {
+
+    browserSync.init({
+        proxy: 'yourlocaldomainhere'
+    });
+
+    gulp.watch('./src/scss/**/*.scss', ['sass']);
+    gulp.watch('./src/js/*.js', ['script']);
+    gulp.watch('./src/images/*', ['imagemin']);
+    gulp.watch('./**/**/*.html.twig').on('change', browserSync.reload);
+});
